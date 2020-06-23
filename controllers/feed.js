b594d98bca81bcf92817428a03586f1dba142ccb
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-23 19:03:29 -0300

    C25A373 - Fetching Lists of Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,9 +1,15 @@
 module.exports.getPosts = (req, res, next) => {
     res.status(200).json({ 
         posts: [{
+            _id: '1',
             title: 'First post',
-            content: 'This is the first post'
-        }] 
+            content: 'This is the first post',
+            imageUrl: 'images/rubber_duck.jpg',
+            creator: {
+                name: 'Márcio Casagrande'
+            },
+            createdAt: new Date()
+        }]
     });
 };
 
