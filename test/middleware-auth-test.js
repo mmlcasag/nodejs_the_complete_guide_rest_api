196e0371b2052b95ea3b9ad6b51f1f930ad23a82
Marcio Casagrande <mmlcasag@gmail.com>
2020-07-03 16:36:43 -0300

    C30A469 - Using Stubs

--- test/middleware-auth-test.js
+++ test/middleware-auth-test.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect;
+const sinon = require('sinon');
+const jwt = require('jsonwebtoken');
 
 const authMiddleware = require('../middlewares/auth');
 
@@ -40,7 +42,7 @@ describe('/middlewares/auth', function() {
     });
     
     // test #4
-    it('should throw an error saying the token is invalid', function() {
+    it('should throw an error if the token cannot be verified', function() {
         const req = {
             headers: {
                 authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZWZlOGRiMTU0YjhmNjJmYTAwMmFjZTAiLCJlbWFpbCI6ImFiYnlickBnbWFpbC5jb20iLCJpYXQiOjE1OTM3NDA3MzksImV4cCI6MTU5Mzc0NDMzOX0.4RylkK5XdNbS_LnG5_f9fGBnE-PDH7xgGIuPNbwnYwQ'
@@ -48,5 +50,61 @@ describe('/middlewares/auth', function() {
         }
         
         expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Invalid token');
+    });
+
+    // test #5
+    /*
+    it('should yield an userId after decoding the token', function() {
+        const req = {
+            headers: {
+                authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZWZlOGRiMTU0YjhmNjJmYTAwMmFjZTAiLCJlbWFpbCI6ImFiYnlickBnbWFpbC5jb20iLCJpYXQiOjE1OTM3NDA3MzksImV4cCI6MTU5Mzc0NDMzOX0.4RylkK5XdNbS_LnG5_f9fGBnE-PDH7xgGIuPNbwnYwQ'
+            }
+        }
+        // how to force the authorization to pass?
+        // by overrinding the jwt.verify method, of course!
+        jwt.verify = function() {
+            return { userId: 'abc' };
+        }
+        authMiddleware(req, {}, () => {});
+        expect(req).to.have.property('userId');
+    });
+    */
+    // the test passed, but the downside of doing that is that
+    // you have replaced the jwt.verify function globally
+    // so if you run another test beneath this line of code
+    // jwt.verify will never do its proper job again
+    // so a more elegant way to solve this problem of mocking
+    // a third-party package is to use sinon
+    // sinon creates stubs, in which it lets you mock a test
+    // and then easily restore the original function so it works
+    // on your other tests
+    // npm install --save-dev sinon
+    // so i will comment the test and re-do it using sinon
+    
+    // test #5
+    it('should yield an userId after decoding the token', function() {
+        const req = {
+            headers: {
+                authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZWZlOGRiMTU0YjhmNjJmYTAwMmFjZTAiLCJlbWFpbCI6ImFiYnlickBnbWFpbC5jb20iLCJpYXQiOjE1OTM3NDA3MzksImV4cCI6MTU5Mzc0NDMzOX0.4RylkK5XdNbS_LnG5_f9fGBnE-PDH7xgGIuPNbwnYwQ'
+            }
+        }
+        // how to force the authorization to pass?
+        // using sinon, of course!
+        // here we pass the package we want to mock and also the name of the function
+        // sinon will replace it with almost an empty function
+        sinon.stub(jwt, 'verify');
+        // almost because it adds this returns function in which you specify what you want it to return
+        jwt.verify.returns({
+            userId: 'abc'
+        });
+        
+        authMiddleware(req, {}, () => {});
+
+        expect(jwt.verify.called).to.be.true;
+        expect(req).to.have.property('userId');
+        
+        // now, after running our test and after we are done with it
+        // we can restore to the original content
+        jwt.verify.restore();
     });
 });
